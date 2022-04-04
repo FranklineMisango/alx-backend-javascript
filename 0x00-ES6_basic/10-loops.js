ca54a0f7566ad921edf43144619a599b9adba280
Virgo-Alpha <bensonkingori96@gmail.com>
2022-04-04 20:24:13 +0400

    sj

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,6 @@ export default function appendToEachArrayValue(array, appendString) {
   const myArray = [];
   for (const idx of array) {
     myArray.push(appendString + idx)
-    }
-
+    };
   return myArray;
 }
