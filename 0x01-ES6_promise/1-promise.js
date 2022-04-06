1240dc3126d1506d09115d857a3bd7d1dccbe446
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2022-04-06 20:55:43 +0400

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,11 +2,10 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
-        status:200,
+        status: 200,
         body: 'Success',
           });
       }
-      reject('The fake API is not working currently');
-      }
-  )
+      reject(new Error('The fake API is not working currently'));
+      });
 }
