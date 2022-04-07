0341ff0def4c4228c447b13ced753b7e56eb8765
Virgo Alpha <bensonkingori96@gmail.com>
2022-04-07 20:30:06 +0400

    mistakes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,17 +1,17 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw TypeError('The name must be a string');
-    if (typeof length !== 'number') throw TypeError('The Length must be a number');
-    if (!Array.isArray(students)) throw TypeError('Students must be an array of strings');
+    if (typeof name !== 'string') throw TypeError('name must be a string');
+    if (typeof length !== 'number') throw TypeError('length must be a number');
+    if (!Array.isArray(students)) throw TypeError('students must be an array of strings');
     students.forEach((student) => {
-      if (typeof student !== 'string') throw TypeError('Students must be an array of strings');
-    });
+    if (typeof student !== 'string') throw TypeError('students must be an array of strings');
+  });
 
-    this._name = name;
-    this._length = length;
-    this._students = students;
+  this._students = students;
+  this._length = length;
+  this._name = name;
   }
-
+  
   set name(newName) {
     if (typeof newName !== 'string') throw TypeError('name must be a string');
     this._name = newName;
